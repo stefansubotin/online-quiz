1719716bae060882adb01a30da5ac130d1c27f69
Stefan Subotin <stefanasubotin@gmail.com>
2023-09-30 15:43:01 +0200

    test

--- src/homepage.module.ts
+++ src/homepage.module.ts
@@ -3,10 +3,11 @@ import { HomepageController } from './homepage.controller';
 import { HomepageService as HomepageService } from './homepage.service';
 import { TestModule } from './test.module';
 import { TestController } from './test.controller';
+import { TestService } from './test.service';
 
 @Module({
   imports: [],
   controllers: [HomepageController, TestController],
-  providers: [HomepageService],
+  providers: [HomepageService, TestService],
 })
 export class HomepageModule {}
