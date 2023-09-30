53b6f7f2ff31bb7677b660d2acb2080cfa570115
Stefan Subotin <stefanasubotin@gmail.com>
2023-09-30 15:21:57 +0200

    test

--- src/homepage.module.ts
+++ src/homepage.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { HomepageController } from './homepage.controller';
 import { HomepageService as HomepageService } from './homepage.service';
 import { TestModule } from './test.module';
+import { TestController } from './test.controller';
 
 @Module({
   imports: [TestModule],
