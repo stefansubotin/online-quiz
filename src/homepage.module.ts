a4f6bda669c35c8ffdaea1188180fe842e5527a4
Stefan Subotin <stefanasubotin@gmail.com>
2023-09-30 18:45:39 +0200

    ups again

--- src/homepage.module.ts
+++ src/homepage.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { HomepageController } from './homepage.controller';
 import { HomepageService as HomepageService } from './homepage.service';
 import { UpdateController } from './update.controller';
+import { UpdateService } from './update.service';
+import { ChatController } from './chat.controller';
 
 @Module({
   imports: [],
-  controllers: [HomepageController, UpdateController],
-  providers: [HomepageService],
+  controllers: [HomepageController, UpdateController, ChatController],
+  providers: [HomepageService, UpdateService],
 })
 export class HomepageModule {}
