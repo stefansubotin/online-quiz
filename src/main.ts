5cb034806cdbdb1b190fa5986287bf9965462406
Stefan Subotin <stefanasubotin@gmail.com>
2023-09-30 16:51:42 +0200

    update view dir

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(HomepageModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setBaseViewsDir(join(__dirname, '..', 'src/views'));
   app.setViewEngine('hbs');
   await app.listen(3000);
 }
