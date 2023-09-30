decba4613bc5f194e7284bda8f7cdd2432cdbb7c
Stefan Subotin <stefanasubotin@gmail.com>
2023-09-30 16:49:19 +0200

    hbs test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { HomepageModule } from './homepage.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(HomepageModule);
+  const app = await NestFactory.create<NestExpressApplication>(HomepageModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
   await app.listen(3000);
 }
 bootstrap();
