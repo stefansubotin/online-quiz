fec104b1c57091386e5522dd948fae9c547b9f8d
Stefan Subotin <stefanasubotin@gmail.com>
2023-10-01 11:26:23 +0200

    test

--- src/chat.service.ts
+++ src/chat.service.ts
@@ -8,9 +8,34 @@ import { createReadStream } from 'fs';
 @Injectable()
 export class ChatService {
     chat(){
-        const chat = createReadStream('components/chat.html');
-        var res;
-        chat.pipe(res);
-        return res;
+        return `<div><p>Hello! Please input a handle before typing an message!</p>
+        <label for="userName">Handle: </label>
+        <input type="text" id="userName" name="userName"><br>
+        <p>Input your chat message here</p>
+        <input type="text" id="messageInput" name="messageInput">
+        <button onclick="onSendClick()">Send</button>
+        <div id="chatDiv"></div>
+        <script>
+            function onSendClick(){
+                var url = window.location.href;
+                var urlArray = url.split('/');
+                var cod = 'my-channel';//urlArray[urlArray.length - 1];
+                var mes = document.getElementById("messageInput").value;
+                var use = document.getElementById("userName").value;
+                fetch("https://filthy-sweatsuit-boa.cyclic.cloud/update", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        id: "messageInput",
+                        code: cod,
+                        event: "chat",
+                        user: use,
+                        message: mes
+                    }),
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8"
+                    }
+                })
+            };
+        </script><div>`;
     }
  }
